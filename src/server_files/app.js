94a8faf32a6ef4ccdceb2488e2fb7252011173bb
VladimirDoneth <vovan.s.marsa@gmail.com>
2020-04-06 22:50:47 +0300

    To .html page added element BUTTON for enabling/disabling MQTT-sending. To .js script added functionality for this button.

--- src/server_files/app.js
+++ src/server_files/app.js
@@ -1,85 +1,106 @@
 var ws;
+let buttonStatusMQTT;
+let isEnabledMQTT = false;
 
 $(function begin() {
-    // Test the protocol support...
+    $('#id_form_button_enable_mqtt').click(function onClick(e) {
+        if (isEnabledMQTT) {
+            buttonStatusMQTT.value = "DISABLE MQTT (actually ENABLED)"
+            isEnabledMQTT = false;
+        } else {
+            buttonStatusMQTT.value = "ENABLE MQTT (actually DISABLED)"
+            isEnabledMQTT = true;
+        }
+    })
+
+    buttonStatusMQTT = document.getElementById('id_button_enable_mqtt');
     ws = new WebSocket('ws://' + document.location.host + '/chart', ['string', 'foo']);
     ws.onopen = function () {
         console.log('onopen');
+        ws.send("GIVE_MQTT_STATUS")
     };
 
     ws.onclose = function () {
-        //add('Lost connection.');
         console.log('onclose');
     };
 
     ws.onmessage = function (message) {
-        //add(message.data);
-        var json_obj = JSON.parse(message.data);
+        var message_data = message.data;
+        if (message_data === "MQTT_STATUS_ENABLE") {
+            isEnabledMQTT = true;
+            buttonStatusMQTT.value = "DISABLE MQTT (actually ENABLED)"
+        } if(message_data === "MQTT_STATUS_DISABLE"){
+            isEnabledMQTT = false;
+            buttonStatusMQTT.value = "ENABLE MQTT (actually DISABLED)"
+        }else {
 
-        let valueAccelX = json_obj.accelX;
-        let valueAccelY = json_obj.accelY;
-        let valueAccelZ = json_obj.accelZ;
-        if (Number.isFinite(valueAccelX) && Number.isFinite(valueAccelY) && Number.isFinite(valueAccelZ)){
-            dataAccelX.unshift(valueAccelX);
-            dataAccelY.unshift(valueAccelY);
-            dataAccelZ.unshift(valueAccelZ);
-            if (dataAccelX > 20) {
-                dataAccelX.pop();
-                dataAccelY.pop();
-                dataAccelZ.pop();
-            }
-            chartAccel.update();
-        }
+            var json_obj = JSON.parse(message_data);
 
-        let valueGyroX = json_obj.gyroX;
-        let valueGyroY = json_obj.gyroY;
-        let valueGyroZ = json_obj.gyroZ;
-        if (Number.isFinite(valueGyroX) && Number.isFinite(valueGyroY) && Number.isFinite(valueGyroZ)){
-            dataGyroX.unshift(valueGyroX);
-            dataGyroY.unshift(valueGyroY);
-            dataGyroZ.unshift(valueGyroZ);
-            if (dataGyroX > 20) {
-                dataGyroX.pop();
-                dataGyroY.pop();
-                dataGyroZ.pop();
+            let valueAccelX = json_obj.accelX;
+            let valueAccelY = json_obj.accelY;
+            let valueAccelZ = json_obj.accelZ;
+            if (Number.isFinite(valueAccelX) && Number.isFinite(valueAccelY) && Number.isFinite(valueAccelZ)) {
+                dataAccelX.unshift(valueAccelX);
+                dataAccelY.unshift(valueAccelY);
+                dataAccelZ.unshift(valueAccelZ);
+                if (dataAccelX > 20) {
+                    dataAccelX.pop();
+                    dataAccelY.pop();
+                    dataAccelZ.pop();
+                }
+                chartAccel.update();
             }
-            chartGyro.update();
-        }
 
-        let valueT = json_obj.temperature;
-        if (Number.isFinite(valueT)) {
-            dataTemperature.unshift(valueT);
-            if (dataTemperature.length > 20) {
-                dataTemperature.pop();
+            let valueGyroX = json_obj.gyroX;
+            let valueGyroY = json_obj.gyroY;
+            let valueGyroZ = json_obj.gyroZ;
+            if (Number.isFinite(valueGyroX) && Number.isFinite(valueGyroY) && Number.isFinite(valueGyroZ)) {
+                dataGyroX.unshift(valueGyroX);
+                dataGyroY.unshift(valueGyroY);
+                dataGyroZ.unshift(valueGyroZ);
+                if (dataGyroX > 20) {
+                    dataGyroX.pop();
+                    dataGyroY.pop();
+                    dataGyroZ.pop();
+                }
+                chartGyro.update();
             }
-            chartTemperature.update();
-        }
 
-        let valueP = json_obj.pressure;
-        if (Number.isFinite(valueP)) {
-            dataPressure.unshift(valueP);
-            if (dataPressure.length > 20) {
-                dataPressure.pop();
+            let valueT = json_obj.temperature;
+            if (Number.isFinite(valueT)) {
+                dataTemperature.unshift(valueT);
+                if (dataTemperature.length > 20) {
+                    dataTemperature.pop();
+                }
+                chartTemperature.update();
             }
-            chartPressure.update();
-        }
 
-        let valueH = json_obj.humidity;
-        if (Number.isFinite(valueH)) {
-            dataHumidity.unshift(valueH);
-            if (dataPressure.length > 20) {
-                dataHumidity.pop();
+            let valueP = json_obj.pressure;
+            if (Number.isFinite(valueP)) {
+                dataPressure.unshift(valueP);
+                if (dataPressure.length > 20) {
+                    dataPressure.pop();
+                }
+                chartPressure.update();
             }
-            chartHumidity.update();
-        }
 
-        let valueA = json_obj.altitude;
-        if (Number.isFinite(valueA)) {
-            dataAltitude.unshift(valueA);
-            if (dataAltitude.length > 20) {
-                dataAltitude.pop();
+            let valueH = json_obj.humidity;
+            if (Number.isFinite(valueH)) {
+                dataHumidity.unshift(valueH);
+                if (dataPressure.length > 20) {
+                    dataHumidity.pop();
+                }
+                chartHumidity.update();
             }
-            chartAltitude.update();
+
+            let valueA = json_obj.altitude;
+            if (Number.isFinite(valueA)) {
+                dataAltitude.unshift(valueA);
+                if (dataAltitude.length > 20) {
+                    dataAltitude.pop();
+                }
+                chartAltitude.update();
+            }
         }
     };
 
